1412f71ad44f5736c893a4f7db6c7afe5acdb4d8
Mark-Kitur <kimutai.workspace@gmail.com>
2025-05-10 13:22:24 +0300

    Implement vowel counting function and add test case

--- problems/easy/vowels/vowels.py
+++ problems/easy/vowels/vowels.py
@@ -1,5 +1,21 @@
 def count_vowels( word):
-    
-    vowels = 0
-    # write your code here
-    return vowels
+    read= list(word)
+    vowels= ['a','e','i','o','u']
+    count =0
+    for i in read:
+        if i in vowels:
+            count +=1
+    return count
+
+# read= "Markaeio"
+# see=list(read)
+# vowels= ['a','e','i','o','u']
+# count=0
+# for i in see:
+#     if i in vowels:
+#         count+=1
+# print(count)
+# print(see)
+
+see= count_vowels("eenbiop")
+print(see)
