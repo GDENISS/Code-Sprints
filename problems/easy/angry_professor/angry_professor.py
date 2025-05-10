ae484df16708e287f3e7660c27f22f5bdaf27f14
GDENISS <mwangi.dennis21@students.dkut.ac.ke>
2025-05-10 13:06:29 +0300

    angry_proff

--- problems/easy/angry_professor/angry_professor.py
+++ problems/easy/angry_professor/angry_professor.py
@@ -1,4 +1,11 @@
-
 def angryProfessor(k, a):
-    # Write your code here
-    return "YES"
+    on_time_count = 0
+    for arrival in a:  # Bug: 'arrival_times' is undefined
+        if arrival <= 0:
+            on_time_count += 1
+    return "YES" if on_time_count < k else "NO"
+
+k = 3
+a = [-1, -3, 4, 2]
+
+print(angryProfessor(k, a))
