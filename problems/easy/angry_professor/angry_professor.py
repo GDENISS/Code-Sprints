0f8c3bd62b41454c2acfd45c8f3bf35bca7882fb
Githinji_Mwangi <114339007+GDENISS@users.noreply.github.com>
2025-05-10 13:13:11 +0300

    Update angry_professor.py

--- problems/easy/angry_professor/angry_professor.py
+++ problems/easy/angry_professor/angry_professor.py
@@ -8,9 +8,3 @@ def angryProfessor(k, a):
         if arrival <= 0:
             on_time_count += 1
     return "YES" if on_time_count < k else "NO"
-
-k = 3
-a = [-1, -3, 4, 2]
-
-print(angryProfessor(k, a))
-
